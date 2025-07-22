7b00e568edc5804b19a754bad2ad52bb4ffab3ae
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-07-22 11:46:22 -0600

    Use leaf instead of use-package

--- init.el
+++ init.el
@@ -99,7 +99,7 @@
 
 ;; which-key: shows a popup of available keybindings when typing a long key
 ;; sequence (e.g. C-x ...)
-(use-package which-key
+(leaf which-key
   :ensure t
   :config
   (which-key-mode))
