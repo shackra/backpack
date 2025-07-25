b774f3387713778a098de8eae368ddc8adab7a7e
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-07-25 06:39:32 -0600

    Remove default bck and frg colors from default-frame-alist

--- early-init.el
+++ early-init.el
@@ -37,9 +37,5 @@
                             ;; (vertical-scroll-bars . nil)
                             ;; (horizontal-scroll-bars . nil)
 
-                            ;; Setting the face in here prevents flashes of
-                            ;; color as the theme gets activated
-                            (background-color . "#000000")
-                            (foreground-color . "#ffffff")
                             (ns-appearance . dark)
                             (ns-transparent-titlebar . t)))
