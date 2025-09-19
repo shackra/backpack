f59b3e7739bbb24cc1cbe4568b86f8b4c2102824
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-09-19 00:02:44 -0600

    chg: install basic packages as Git submodules
    leaf.el and elpaca are the basis of this Emacs distribution

--- early-init.el
+++ early-init.el
@@ -1,1 +1,19 @@
 (setq package-enable-at-startup nil)
+
+(defvar elpaca-installer-version 0.11)
+(defvar elpaca-directory (expand-file-name ".cache/elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+
+(add-to-list 'load-path (expand-file-name "base-packages/elpaca" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "base-packages/leaf.el" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "base-packages/leaf-keywords.el" user-emacs-directory))
+
+(require 'elpaca)
+(require 'leaf)
+(require 'leaf-keywords)
+
+(leaf-keywords-init)
+
+(setq leaf-alias-keyword-alist '((:ensure . :elpaca)))
+(add-hook 'after-init-hook #'elpaca-process-queues)
