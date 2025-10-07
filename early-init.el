bd0925b6e8b19ae9d2346779591a42222968ea79
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-10-06 22:14:31 -0600

    chg: Use elpaca installation bootstrap script

--- early-init.el
+++ early-init.el
@@ -6,7 +6,6 @@
 
 ;; directories
 (defvar backpack--base-packages-dir (expand-file-name "base-packages" user-emacs-directory))
-(defvar backpack--elpaca (expand-file-name "elpaca" backpack--base-packages-dir))
 (defvar backpack--leaf (expand-file-name "leaf.el" backpack--base-packages-dir))
 (defvar backpack--leaf-keywords (expand-file-name "leaf-keywords.el" backpack--base-packages-dir))
 
@@ -46,21 +45,40 @@
 (defvar elpaca-directory (expand-file-name ".cache/elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-
-;; this is a slimmed down version of the bootstrap code offered in the readme
-;; due to shipping elpaca already as a Git submodule
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1 :inherit ignore
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (default-directory repo)) ;; everything happens in the repo directory
+       (order (cdr elpaca-order))
+       (default-directory repo))
   (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo) 		;; copy the Git submodule to ~/.emacs.d/.cache/elpaca/repo/elpaca
-    (copy-directory backpack--elpaca repo t t t))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (<= emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
     (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
-
 (add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
 
 (require 'leaf)
 (require 'leaf-keywords)
