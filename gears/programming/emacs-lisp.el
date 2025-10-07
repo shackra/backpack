9b8731d73706dbe353721cf9fd1d177ea284adf7
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-10-07 11:09:05 -0600

    fix: Don't customize common-lisp hook on emacs-lisp leaf

--- gears/programming/emacs-lisp.el
+++ gears/programming/emacs-lisp.el
@@ -6,7 +6,7 @@
   :config
   (leaf lispy
     :ensure (lispy :host github :repo "enzuru/lispy" :ref "c2acc4bfc1add60c8679c1191fa1b10144f748eb")
-    :hook ((emacs-lisp-mode-hook lisp-mode-hook lisp-interaction-mode-hook common-lisp-mode-hook) . lispy-mode)
+    :hook ((emacs-lisp-mode-hook lisp-mode-hook lisp-interaction-mode-hook) . lispy-mode)
     :setq
     (lispy-key-theme . '(special paredit c-digits))
     (lispy-close-quotes-at-end-p . t)
