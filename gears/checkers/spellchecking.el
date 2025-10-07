39eb256826f61c618779640e57c9f3485909ad03
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-10-07 15:32:17 -0600

    fix: Change jinx-correct binding as it colides with iedit

--- gears/checkers/spellchecking.el
+++ gears/checkers/spellchecking.el
@@ -5,7 +5,7 @@
   :ensure (jinx :ref "55c844066601ec1a4741ff8f459ff6321f6935b7")
   :when (gearp! :checkers spellchecking)
   :hook ((text-mode-hook prog-mode-hook) . jinx-mode)
-  :bind ("C-;" . jinx-correct)
+  :bind ("C-c ." . jinx-correct)
   :custom
   (jinx-camel-modes . '(prog-mode))
   (jinx-delay . 0.01))
