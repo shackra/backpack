0cc0e278df8347e90f2c74ec8df2f799e8abd2e9
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-10-07 13:54:00 -0600

    new: Add org-mode

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 (load (expand-file-name "gears/checkers/spellchecking.el" user-emacs-directory))
 (load (expand-file-name "gears/programming/emacs-lisp.el" user-emacs-directory))
 (load (expand-file-name "gears/programming/go.el" user-emacs-directory))
+(load (expand-file-name "gears/programming/org.el" user-emacs-directory))
 
 ;; shamelessly copied from Emacs Bedrock (leave this at the end of the file, always)
 (setq gc-cons-threshold (or backpack--initial-gc-threshold 800000))
