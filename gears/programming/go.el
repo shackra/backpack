8b72eded7e764108806028afcbf501743d0a9af8
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-10-07 13:22:58 -0600

    new: Add support for the Go programming language

---
+++ gears/programming/go.el
@@ -0,0 +1,25 @@
+(require 'backpack-pouch)
+
+(leaf go-mode
+  :doc "Support for Go programming language in Emacs"
+  :when (gearp! :programming go)
+  :ensure (go-mode :ref "0ed3c5227e7f622589f1411b4939c3ee34711ebd")
+  :config
+  (leaf eglot
+    :doc "Language Server Protocol support for go-mode"
+    :when (gearp! :programming go lsp)
+    :doctor ("gopls" . "the official language server protocol (lsp) implementation provided by the Go team")
+    :hook (go-mode-hook . eglot-ensure)
+    :config
+    (add-to-list 'eglot-server-programs '(go-mode . ("gopls" "serve"))))
+
+  (leaf go-impl
+    :doc "generates method stubs for implementing an interface"
+    :when (gearp! :programming go impl)
+    :ensure (go-impl :ref "1eebba6ccd02d11a5a82ad4540a8d562797bc3b3")
+    :doctor ("impl" . "command that generates method stubs for implementing an interface"))
+
+  (leaf go-rename
+    :doc "Integration of the 'gorename' tool into Emacs"
+    :when (gearp! :programming go rename)
+    :doctor ("gorename" . "command that performs precise type-safe renaming of identifiers in Go source code")))
--- init.el
+++ init.el
@@ -8,6 +8,7 @@
 (load (expand-file-name "gears/tools/magit.el" user-emacs-directory))
 (load (expand-file-name "gears/checkers/spellchecking.el" user-emacs-directory))
 (load (expand-file-name "gears/programming/emacs-lisp.el" user-emacs-directory))
+(load (expand-file-name "gears/programming/go.el" user-emacs-directory))
 
 ;; shamelessly copied from Emacs Bedrock (leave this at the end of the file, always)
 (setq gc-cons-threshold (or backpack--initial-gc-threshold 800000))
