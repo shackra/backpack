e2d38468c31ceae30ee40a93216126e47088b003
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-10-08 18:42:22 -0600

    fix: Document orderless' leaf

--- gears/completion/orderless.el
+++ gears/completion/orderless.el
@@ -1,6 +1,7 @@
 (require 'backpack-pouch)
 
 (leaf orderless
+  :doc "an orderless «completion style» that divides the pattern into space-separated components, and matches candidates that match all of the components in any order"
   :unless (gearp! :completion -orderless)
   :ensure (orderless :ref "9cf1c90e2501566ceba59f3220b4630995004efd")
   :custom
