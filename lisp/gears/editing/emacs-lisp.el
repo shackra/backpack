420b30b4e8b805a9f538c2aad3b9b197f2bab18d
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-10-27 02:22:11 -0600

    fix: Separate lispy from Emacs Lisp leaf

--- lisp/gears/editing/emacs-lisp.el
+++ lisp/gears/editing/emacs-lisp.el
@@ -7,15 +7,16 @@
      (toggle-truncate-lines +1)
 
      (unless (gearp! :editing emacs-lisp -display-line-numbers)
-       (display-line-numbers-mode +1))))
-  :config
-  (leaf lispy
-    :ensure (lispy :host github :repo "enzuru/lispy" :ref "c2acc4bfc1add60c8679c1191fa1b10144f748eb")
-    :hook ((emacs-lisp-mode-hook lisp-mode-hook lisp-interaction-mode-hook) . lispy-mode)
-    :setq
-    (lispy-key-theme . '(special paredit c-digits))
-    (lispy-close-quotes-at-end-p . t)
-    (lispy-safe-copy . t)
-    (lispy-safe-delete . t)
-    (lispy-safe-paste . t)
-    (lispy-delete-sexp-from-within . nil)))
+       (display-line-numbers-mode +1)))))
+
+(leaf lispy
+  :when (gearp! :editing emacs-lisp)
+  :ensure (lispy :host github :repo "enzuru/lispy" :ref "c2acc4bfc1add60c8679c1191fa1b10144f748eb")
+  :hook ((emacs-lisp-mode-hook lisp-mode-hook lisp-interaction-mode-hook) . lispy-mode)
+  :setq
+  (lispy-key-theme . '(special paredit c-digits))
+  (lispy-close-quotes-at-end-p . t)
+  (lispy-safe-copy . t)
+  (lispy-safe-delete . t)
+  (lispy-safe-paste . t)
+  (lispy-delete-sexp-from-within . nil))
