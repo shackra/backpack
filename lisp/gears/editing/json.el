6e2d6984864af14a8acdbda08dea27d1487aba45
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-10-30 01:44:28 -0600

    new: Add support for JSON

---
+++ lisp/gears/editing/json.el
@@ -0,0 +1,23 @@
+(leaf json
+  :doc "major mode for editing JSON files"
+  :when (gearp! :editing json)
+  :hook
+  (js-json-mode-hook . electric-pair-local-mode)
+  (js-json-mode-hook .
+		  (lambda ()
+		    (toggle-truncate-lines 1)
+		    (unless (gearp! :editing json -display-line-numbers)
+		      (display-line-numbers-mode +1))))
+  :config
+  (leaf eglot
+    :when (gearp! :editing json lsp)
+    :doc "Language Server Protocol support for JSON"
+    :hook
+    (js-json-mode-hook . eglot-ensure)))
+
+(leaf json-ts-mode
+  :doc "treesit support for editing JSON files"
+  :when (and (gearp! :editing json) (not (gearp! :editing json -treesit)))
+  :config
+  (add-to-list 'major-mode-remap-alist '(js-json-mode . json-ts-mode))
+  (setq json-ts-mode-hook js-json-mode-hook))
