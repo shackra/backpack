aa68576ece9a482d03f8a8555473c1720df79fa9
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-02-01 20:09:05 -0600

    fix: Require treesit-auto

--- lisp/gears/ui/treesit.el
+++ lisp/gears/ui/treesit.el
@@ -8,6 +8,7 @@
   :doc "activate treesit everywhere"
   :unless (gearp! :ui -treesit)
   :ensure (treesit-auto :ref "016bd286a1ba4628f833a626f8b9d497882ecdf3")
+  :require t
   :advice
   (:around treesit-install-language-grammar
            (lambda (orig-fun lang &optional out-dir)
