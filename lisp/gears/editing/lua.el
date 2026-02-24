2a2a8daaa2f667c834a51d8ab8937cf15a6a5a5f
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-02-24 12:05:13 -0600

    fix: Lua: define ts-mode-hook as well

--- lisp/gears/editing/lua.el
+++ lisp/gears/editing/lua.el
@@ -10,21 +10,19 @@
   :ensure (lua-mode :ref "2f6b8d7a6317e42c953c5119b0119ddb337e0a5f")
   :when (gearp! :editing lua)
   :hook
-  (lua-mode-hook . electric-pair-local-mode)
-  (lua-mode-hook .
-		 (lambda ()
-		   (toggle-truncate-lines +1)
-		   (unless (gearp! :editing lua -display-line-numbers)
-		     (display-line-numbers-mode +1))))
+  ((lua-mode-hook lua-ts-mode-hook) . electric-pair-local-mode)
+  ((lua-mode-hook lua-ts-mode-hook) .
+   (lambda ()
+     (toggle-truncate-lines +1)
+     (unless (gearp! :editing lua -display-line-numbers)
+       (display-line-numbers-mode +1))))
   :config
   (leaf eglot
     :doc "Language Server Protocol support for lua-mode"
     :when (gearp! :editing lua lsp)
     :doctor
     ("lua-language-server" . "provides various language features for Lua to make development easier and faster")
-    :hook (lua-mode-hook . eglot-ensure)
+    :hook ((lua-mode-hook lua-ts-mode-hook) . eglot-ensure)
     :config
-    (add-to-list 'eglot-server-programs '(lua-mode . ("lua-language-server"))))
-
-  (unless (gearp! :editing lua -treesit)
-    (setq lua-ts-mode-hook lua-mode-hook)))
+    (add-to-list 'eglot-server-programs '(lua-mode . ("lua-language-server")))
+    (add-to-list 'eglot-server-programs '(lua-ts-mode . ("lua-language-server")))))
