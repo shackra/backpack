561168c03f855c38549940abcb705acf699fc4dc
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-02-24 17:56:58 -0600

    fix: Set the appropriate git commit SHAs

--- lisp/gears/editing/rust.el
+++ lisp/gears/editing/rust.el
@@ -7,7 +7,7 @@
 
 (leaf rust-mode
   :doc "A major-mode for editing Rust source code"
-  :ensure (rust-mode :ref "d00d83d3a207a5b7c2994392b2781f627e3159ce")
+  :ensure (rust-mode :ref "f68ddca5c22b94a2de7c9ce20d629cd78d60b269")
   :when (gearp! :editing rust)
   :hook
   ((rust-mode-hook rust-ts-mode-hook) . electric-pair-local-mode)
@@ -27,18 +27,19 @@
     (add-to-list 'eglot-server-programs '(rust-mode . ("rust-analyzer")))
     (add-to-list 'eglot-server-programs '(rust-ts-mode . ("rust-analyzer"))))
 
-  (leaf cargo
+  (leaf cargo-mode
     :doc "Cargo process support for Emacs"
     :when (gearp! :editing rust cargo)
-    :ensure (cargo :ref "5f5a7b5bd4bf671d3a00c0523e6946c9a6f0a509")
+    :ensure (cargo-mode :ref "33528954218f8957a26f3fef506c3537823d569d")
     :doctor ("cargo" . "the Rust package manager")
+    :custom (compilation-scroll-output . t)
     :hook ((rust-mode-hook rust-ts-mode-hook) . cargo-minor-mode)))
 
 (leaf ob-rust
   :doc "Rust source blocks in org-mode"
   :when (and (gearp! :editing rust) (gearp! :editing org))
   :after org
-  :ensure (ob-rust :ref "d0fab5ec33a31413643124c744da10bfbed4ef01")
+  :ensure (ob-rust :ref "be059d231fafeb24a658db212a55ccdc55c0c500")
   :doctor ("rustc" . "the Rust compiler")
   :config
   (org-babel-do-load-languages
