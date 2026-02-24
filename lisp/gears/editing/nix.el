d0514bd8581a88f74965d8d7035b19023e1413c8
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-02-24 12:09:12 -0600

    fix: Nix: define ts-mode-hook as well

--- lisp/gears/editing/nix.el
+++ lisp/gears/editing/nix.el
@@ -10,33 +10,36 @@
   :ensure (nix-mode :ref "719feb7868fb567ecfe5578f6119892c771ac5e5")
   :when (gearp! :editing nix)
   :hook
-  (nix-mode-hook . electric-pair-local-mode)
-  (nix-mode-hook . (lambda ()
-		     (toggle-truncate-lines +1)
-		     (unless (gearp! :editing nix -display-line-numbers)
-		       (display-line-numbers-mode +1)))))
+  ((nix-mode-hook nix-ts-mode-hook) . electric-pair-local-mode)
+  ((nix-mode-hook nix-ts-mode-hook) .
+   (lambda ()
+     (toggle-truncate-lines +1)
+     (unless (gearp! :editing nix -display-line-numbers)
+       (display-line-numbers-mode +1)))))
 
 (leaf eglot
-    :doc "Language Server Protocol support for nix-mode"
-    :when (and (gearp! :editing nix lsp) (gearp! :editing nix))
-    :doctor
-    ("nixd" . "a feature-rich nix language server interoperating with C++ nix")
-    ("nil"  . "an incremental analysis assistant for writing in Nix")
-    ("nixfmt" . "format Nix source code, used with your LSP server of choice")
-    ("alejandra" . "the Uncompromising Nix Code Formatter, used with your LSP server of choice")
-    :hook
-    (nix-mode-hook . eglot-ensure)
-    :config
-    (add-to-list 'eglot-server-programs
-		 `(nix-mode . ,(eglot-alternatives '(("nixd")
+  :doc "Language Server Protocol support for nix-mode"
+  :when (and (gearp! :editing nix lsp) (gearp! :editing nix))
+  :doctor
+  ("nixd" . "a feature-rich nix language server interoperating with C++ nix")
+  ("nil"  . "an incremental analysis assistant for writing in Nix")
+  ("nixfmt" . "format Nix source code, used with your LSP server of choice")
+  ("alejandra" . "the Uncompromising Nix Code Formatter, used with your LSP server of choice")
+  :hook
+  ((nix-mode-hook nix-ts-mode-hook) . eglot-ensure)
+  :config
+  (add-to-list 'eglot-server-programs
+	       `(nix-mode . ,(eglot-alternatives '(("nixd")
+						   ("nil" "--stdio")))))
+  (add-to-list 'eglot-server-programs
+		 `(nix-ts-mode . ,(eglot-alternatives '(("nixd")
 						     ("nil" "--stdio"))))))
 
 (leaf nix-ts-mode
   :doc "tree-sitter major mode for nix"
   :ensure (nix-ts-mode :ref "d769e53ccc0f40026fd11c7e23bf419c2caf4732")
   :unless (gearp! :editing nix -treesit)
-  :config
-  (setq nix-ts-mode-hook nix-mode-hook))
+  :after nix-mode)
 
 (leaf ob-nix
   :doc "a simple org-babel language extension to evaluate nix expressions using `nix-instantiate`"
