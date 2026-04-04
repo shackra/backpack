195eb895a5a28f6ce9158defdb600d9bd47dcea3
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-04-03 19:27:13 -0600

    fix: Enable Tree-sitter support in Markdown

--- lisp/gears/editing/markdown.el
+++ lisp/gears/editing/markdown.el
@@ -1,10 +1,24 @@
+;; Declare tree-sitter languages needed by this gear
+(when (and (gearp! :editing markdown)
+           (not (gearp! :editing markdown -treesit)))
+  (backpack-treesit-langs! markdown)
+
+  (add-to-list 'major-mode-remap-alist '(markdown-mode . markdown-ts-mode)))
+
 (leaf markdown-mode
   :doc "the markup language everyone thinks they know until they hit nested lists"
   :when (gearp! :editing markdown)
   :ensure (markdown-mode :ref "9de2df5a9f2f864c82ec112d3369154767a2bb49")
   :mode ("\\.md\\'" "\\.markdown\\'" "\\.mkd\\'")
   :hook
-  (markdown-mode-hook . visual-line-mode)
+  ((markdown-mode-hook markdown-ts-mode-hook) . visual-line-mode)
   :config
   (when (gearp! :editing markdown display-line-numbers)
-    (add-hook 'markdown-mode-hook #'display-line-numbers-mode)))
+    (add-hook 'markdown-mode-hook #'display-line-numbers-mode)
+    (add-hook 'markdown-ts-mode-hook #'display-line-numbers-mode)))
+
+(leaf markdown-ts-mode
+  :doc "tree-sitter support for Markdown"
+  :ensure (markdown-ts-mode :ref "2f1ee8b94cdf53cebc31ae08ecfbba846193d5e1")
+  :unless (gearp! :editing markdown -treesit)
+  :after markdown-mode)
