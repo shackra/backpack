e002cb626eb571e19c6b25e8cccc2cf0edae8e25
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-04-10 00:04:27 -0600

    fix: Copy emacs-lisp-mode configuration for lisp-data-mode

--- lisp/gears/editing/emacs-lisp.el
+++ lisp/gears/editing/emacs-lisp.el
@@ -2,7 +2,7 @@
   :when (gearp! :editing emacs-lisp)
   :doc "the language you swore you'd never learn, and now you can't stop"
   :hook
-  ((emacs-lisp-mode-hook lisp-mode-hook) .
+  ((emacs-lisp-mode-hook lisp-mode-hook lisp-data-mode-hook) .
    (lambda ()
      (toggle-truncate-lines +1)
 
@@ -12,7 +12,7 @@
 (leaf lispy
   :when (gearp! :editing emacs-lisp)
   :ensure (lispy :host github :repo "enzuru/lispy" :ref "c2acc4bfc1add60c8679c1191fa1b10144f748eb")
-  :hook ((emacs-lisp-mode-hook lisp-mode-hook lisp-interaction-mode-hook) . lispy-mode)
+  :hook ((emacs-lisp-mode-hook lisp-mode-hook lisp-interaction-mode-hook lisp-data-mode-hook) . lispy-mode)
   :setq
   (lispy-key-theme . '(special paredit c-digits))
   (lispy-close-quotes-at-end-p . t)
@@ -25,7 +25,7 @@
   :doc "Let-binding values in Elisp completion"
   :when (gearp! :editing emacs-lisp)
   :ensure (let-completion :host github :repo "gggion/let-completion.el" :ref "ee8822e32ba9c0a81152a5dd9bf4e272655d7319")
-  :hook ((emacs-lisp-mode-hook lisp-mode-hook lisp-interaction-mode-hook) . let-completion-mode)
+  :hook ((emacs-lisp-mode-hook lisp-mode-hook lisp-interaction-mode-hook lisp-data-mode-hook) . let-completion-mode)
   :setq let-completion-tag-kind-alist
   '((lambda              . "λ")
     (cl-function         . "𝘧")
