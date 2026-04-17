990835875f1c01a68b4e579f8fc6be58b236f539
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-04-17 01:18:40 -0600

    fix: Switch eldoc-box to something else when running in terminal

--- lisp/gears/tools/eldoc.el
+++ lisp/gears/tools/eldoc.el
@@ -4,9 +4,24 @@
   :global-minor-mode global-eldoc-mode)
 
 (leaf eldoc-box
-  :when (gearp! :tools eldoc box)
+  :doc "Display eldoc documentation in a childframe at point. GUI only.
+In terminal, use C-h . to show documentation in a side window."
+  :when (and (gearp! :tools eldoc box) (display-graphic-p))
   :ensure (eldoc-box :ref "fead2cef661790417267e5498d4d14806e020f99")
   :custom
   (eldoc-box-hover-display-frame-above-point . t)
   :hook
   (eldoc-mode-hook . eldoc-box-hover-at-point-mode))
+
+(when (and (gearp! :tools eldoc box) (not (display-graphic-p)))
+  (setq eldoc-echo-area-use-multiline-p nil)
+  (setq eldoc-echo-area-prefer-doc-buffer 'maybe)
+  (setq eldoc-echo-area-display-truncation-message t)
+  (add-to-list 'display-buffer-alist
+               '("\\*eldoc\\*"
+                 (display-buffer-in-side-window)
+                 (side . bottom)
+                 (window-height . 0.15)
+                 (slot . 0)
+                 (window-parameters . ((no-other-window . t)
+                                       (no-delete-other-windows . t))))))
