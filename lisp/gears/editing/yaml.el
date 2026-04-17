d4f609ad481fc4cb284af2d9273aa6753e13ffd8
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-04-17 14:36:59 -0600

    fix: Require yaml-mode so that we can use yaml-indent-line in yaml-ts-mode

--- lisp/gears/editing/yaml.el
+++ lisp/gears/editing/yaml.el
@@ -11,6 +11,7 @@
 (leaf yaml-mode
   :doc "indentation-sensitive and ready to ruin your CI pipeline"
   :ensure (yaml-mode :ref "d91f878729312a6beed77e6637c60497c5786efa")
+  :require t
   :when (gearp! :editing yaml)
   :mode "\\.ya?ml\\'"
   :hook
