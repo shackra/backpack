7d918f40b52d34b1c9c31fcae12e7463f80143f7
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-04-16 20:11:54 -0600

    chore: Configure LSP workspace for YAML

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,14 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((jinx-languages . "en"))))
+((nil . ((jinx-languages . "en")))
+ (yaml-ts-mode . ((eglot-workspace-configuration . (:yaml (
+							   :format (:enable t)
+							   :validate t
+							   :hover t
+							   :completion t
+							   :schemas (
+								     :https\://www.schemastore.org/github-workflow.json "/.github/workflows/ci.yml"
+								     )
+							   :schemaStore (:enable t))
+							  :editor (:tabSize 4))))))
