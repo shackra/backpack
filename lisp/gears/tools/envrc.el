5dbda8be8a00bd37523ac3590310f9f9fe0cf74c
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-05-10 00:13:35 -0600

    fix(direnv): Define advice at :init block of leaf
    otherwise it would be too late for the advice to work

--- lisp/gears/tools/envrc.el
+++ lisp/gears/tools/envrc.el
@@ -2,14 +2,14 @@
   :doc "Emacs support for direnv which operates buffer-locally"
   :when (gearp! :tools direnv)
   :ensure (envrc :ref "de1ae6e538764f74659f358b04af0d84fa0fef42")
-  :global-minor-mode envrc-global-mode
-  :bind-keymap
-  ("C-c e" . envrc-command-map)
-  :config
-  ;; Prevent envrc from activating in user-emacs-directory.
+  ;; Prevent envrc from activating in backpack-emacs-dir.
   ;; Backpack itself is a devenv project, but we do not want its
   ;; .envrc to kick in when Emacs is installed there.
+  :init
   (define-advice envrc-mode (:before-while (&optional arg) backpack--skip-emacs-dir)
-    "Do not enable envrc-mode inside `user-emacs-directory'."
+    "Do not enable envrc-mode inside `backpack-emacs-dir'."
     (or (and (numberp arg) (< arg 1))  ; always allow disabling
-        (not (file-in-directory-p default-directory user-emacs-directory)))))
+        (not (file-in-directory-p default-directory backpack-emacs-dir))))
+  :global-minor-mode envrc-global-mode
+  :bind-keymap
+  ("C-c e" . envrc-command-map))
