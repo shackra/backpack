e942dc577ac6e9879c91cb7d9434a42a1e7ca491
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-05-10 00:02:49 -0600

    fix: Don't activate envrc when Backpack is cloned in the user-emacs-directory

--- lisp/gears/tools/envrc.el
+++ lisp/gears/tools/envrc.el
@@ -4,4 +4,12 @@
   :ensure (envrc :ref "de1ae6e538764f74659f358b04af0d84fa0fef42")
   :global-minor-mode envrc-global-mode
   :bind-keymap
-  ("C-c e" . envrc-command-map))
+  ("C-c e" . envrc-command-map)
+  :config
+  ;; Prevent envrc from activating in user-emacs-directory.
+  ;; Backpack itself is a devenv project, but we do not want its
+  ;; .envrc to kick in when Emacs is installed there.
+  (define-advice envrc-mode (:before-while (&optional arg) backpack--skip-emacs-dir)
+    "Do not enable envrc-mode inside `user-emacs-directory'."
+    (or (and (numberp arg) (< arg 1))  ; always allow disabling
+        (not (file-in-directory-p default-directory user-emacs-directory)))))
