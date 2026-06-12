4df64e71cfcfc5751933aab3e2a5cb426827ca9d
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-06-11 22:12:30 -0600

    fix: Incorrect syntax

--- lisp/gears/ai/eca.el
+++ lisp/gears/ai/eca.el
@@ -7,4 +7,4 @@
   :ensure (eca :host github :repo "editor-code-assistant/eca-emacs"
 	       :ref "0e1c7b4e924d7d7d99720342e60483b6dda187a3"
 	       :files ("*.el"))
-  :doctor ("eca" . "server binary" 'required))
+  :doctor ("eca" . ("server binary" 'required)))
